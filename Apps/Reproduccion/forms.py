e376950de5d9adc12dd5d8d3e1e399d6f3088615
Sergio Vazquez <sergiovazag@gmail.com>
2020-05-19 23:30:58 -0500

    **Added** Token and POST

--- Apps/Reproduccion/forms.py
+++ Apps/Reproduccion/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length = 50, min_length = 5, required = True, widget = forms.TextInput())
-    password = forms.CharField(max_length = 50, min_length = 5, required = True, widget = forms.TextInput())
+    username = forms.CharField(label='Ingresa tu usuario', max_length = 50, min_length = 5, required = True, widget = forms.TextInput(attrs={'placeholder' : 'Ingresa tu usuario', 'class' : 'form-control'}))
+    password = forms.CharField(label='Ingresa tu contraseña',max_length = 50, min_length = 5, required = True, widget = forms.PasswordInput(attrs={'placeholder' : 'Ingresa tu contraseña','class' : 'form-control'}))
--- Apps/Reproduccion/views.py
+++ Apps/Reproduccion/views.py
@@ -5,5 +5,20 @@ from .forms import LoginForm
 
 # Create your views here.
 def home(request):
-    form = LoginForm()
-    return render(request, 'repro_home.html', {'form':form})
+    
+
+    if request.method == 'POST':
+        formS = LoginForm(request.POST)
+        if formS.is_valid():
+            username = formS.cleaned_data['username']
+            password = formS.cleaned_data['password']
+            print(username)
+            print(password)
+            return HttpResponse(username)
+        else:
+            return HttpResponse('Revisa tu formulario')
+
+
+    else:
+        form = LoginForm()
+        return render(request, 'repro_home.html', {'form':form})
